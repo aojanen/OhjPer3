f86a1f9fdb3621b7b7426b2768122df8bc70947b
Akseli Ojanen <akseli.ojanen@tuni.fi>
2024-02-09 18:54:40 +0200

    add wordgame

---
+++ round5/wordgame/src/main/java/fi/tuni/prog3/wordgame/WordGame.java
@@ -0,0 +1,145 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.tuni.prog3.wordgame;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author ttakoj
+ */
+public class WordGame {
+	
+	private ArrayList<String> wordBin;
+	private WordGameState gameState;
+	private String targetWord;
+	private boolean isGameActive;
+	
+	// Constructor
+	public WordGame(String wordFileName) throws IOException{
+		gameState = null;
+		isGameActive = false;
+		wordBin = new ArrayList();
+		try(var input = new Scanner(new File(wordFileName))){
+			while (input.hasNextLine()){
+				String word = input.nextLine();
+				wordBin.add(word);
+			}		
+		}
+	}
+	
+	public class WordGameState {
+		private String word;
+		private int mistakeLimit;
+		private int mistakes;
+		private int missingChars;
+
+		private WordGameState(String word, int mistakeLimit){
+			this.word = "_".repeat(word.length());
+			this.mistakeLimit = mistakeLimit;
+			this.mistakes = 0;
+			this.missingChars = word.length();
+		}
+
+		public String getWord(){
+			return word;
+		}
+		
+		public int getMistakes(){
+			return mistakes;
+		}
+		
+		public int getMistakeLimit(){
+			return mistakeLimit;
+		}
+		
+		public int getMissingChars(){
+			return missingChars;
+		}
+		
+		public void setWord(String word){
+			this.word = word;
+		}
+		
+		public void addMistake(){
+			mistakes++;
+		}
+		
+		public void decMissingChars(){
+			missingChars--;
+		}
+		
+		public void nulMissingChars(){
+			missingChars = 0;
+		}
+	}
+	
+	public void initGame(int wordIndex, int mistakeLimit){
+		targetWord = wordBin.get(wordIndex % wordBin.size());
+		gameState = new WordGameState(targetWord, mistakeLimit);
+		isGameActive = true;
+	}
+	
+	public boolean isGameActive(){
+		return isGameActive;
+	}
+	
+	public WordGameState getGameState() throws GameStateException {
+		if (!isGameActive()){
+			throw new GameStateException("There is currently no active word game!");
+		}
+		
+		return gameState;
+	}
+	
+	public WordGameState guess(char c) throws GameStateException{
+		if (!isGameActive()){
+			throw new GameStateException("There is currently no active word game!");
+		}
+		String g = Character.toString(c).toLowerCase();
+		String tLow = targetWord.toLowerCase();
+		if (tLow.contains(g)) {
+			StringBuilder guessState = new StringBuilder(gameState.getWord());
+			for (int i = 0; i < targetWord.length(); i++){
+				if (tLow.charAt(i) == g.charAt(0)){
+					guessState.setCharAt(i, c);
+					gameState.decMissingChars();
+				}
+				gameState.setWord(guessState.toString());
+			}
+		} else {
+			gameState.addMistake();
+		}
+		
+		if (gameState.getMissingChars() == 0 || gameState.getMistakes() > gameState.getMistakeLimit()){
+			isGameActive = false;
+			gameState.setWord(targetWord);
+			
+		}
+		return gameState;
+	}
+	
+	public WordGameState guess(String g) throws GameStateException{
+		if (!isGameActive()){
+			throw new GameStateException("There is currently no active word game!");
+		}
+		if (g == targetWord){
+			gameState.setWord(g);
+			gameState.nulMissingChars();
+		} else {
+			gameState.addMistake();
+			if (gameState.getMistakes() > gameState.getMistakeLimit()){
+				isGameActive = false;
+				gameState.setWord(targetWord);
+			}
+		}
+		return gameState;
+	}
+}
+	
+
